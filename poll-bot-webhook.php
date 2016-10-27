97ee24cb48fe64e635522509fd1ef077084a8fcb
iammtxd <henrytin329@gmail.com>
2016-10-27 14:48:32 +0800

    Update

--- poll-bot-webhook.php
+++ poll-bot-webhook.php
@@ -2,7 +2,7 @@
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '243977927:AAFYaTxEjr3UdKREGLH60FdV2SQUkuPJjX0');
 define('BOT_WEBHOOK', 'https://bot.server/poll-bot-webhook.php');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
