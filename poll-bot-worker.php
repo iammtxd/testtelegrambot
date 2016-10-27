61ea654566585445149dc6bea88e5d2786a7a6ca
iammtxd <henrytin329@gmail.com>
2016-10-27 14:47:32 +0800

    Update poll-bot-worker.php

--- poll-bot-worker.php
+++ poll-bot-worker.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '243977927:AAFYaTxEjr3UdKREGLH60FdV2SQUkuPJjX0');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
